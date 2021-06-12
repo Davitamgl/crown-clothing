02c2ef01d978f6841f4aae2f44d97b206b23bf6c
Davit Amaghlobeli <amaglobeli53@gmail.com>
2021-06-12 20:19:39 +0400

    added checkout page, checkout item component and updated cart to use reselect selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import { createStructuredSelector } from "reselect";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 
@@ -8,6 +8,8 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 const Header = ({ currentUser, hidden }) => (
   <div className="header">
@@ -33,12 +35,11 @@ const Header = ({ currentUser, hidden }) => (
       <CartIcon />
     </div>
     {hidden ? null : <CartDropdown />}
-    
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 export default connect(mapStateToProps)(Header);
