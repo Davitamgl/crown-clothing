a579e0ed8de6075ef50519e981b321a8c6e16d7f
Davit Amaghlobeli <amaglobeli53@gmail.com>
2021-07-11 19:27:54 +0400

    created user and cart sagas for sign in, sign up and sign in with google

--- src/components/collections-overview/collections-overview.components.jsx
+++ src/components/collections-overview/collections-overview.components.jsx
@@ -1,24 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import CollectionPreview from "../preview-collection/preview-collection.component";
 
-import CollectionPreview from '../preview-collection/preview-collection.component'
-
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
-
-import './collections-overview.styles.scss';
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className='collections-overview'>
+  <CollectionsOverviewContainer>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
