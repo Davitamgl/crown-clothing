39992fd30790b51f5fcb4fee80469ea8ac924064
Davit Amaghlobeli <amaglobeli53@gmail.com>
2021-07-30 21:44:06 +0400

    added performance optimizations to codebase with react lazy, suspense and memo

--- client/src/components/collections-overview/collections-overview.components.jsx
+++ client/src/components/collections-overview/collections-overview.components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from "../preview-collection/preview-collection.component";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import { CollectionsOverviewContainer } from "./collections-overview.styles";
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,19 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
-import CollectionPageContainer from "../collection/collection.container";
+import Spinner from "../../components/spinner/spinner.component";
+
+const CollectionsOverviewContainer = lazy(() => import("../../components/collections-overview/collections-overview.container"));
+const CollectionPageContainer = lazy(() => import("../collection/collection.container"));
+
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
     fetchCollectionsStart();
-  },[fetchCollectionsStart ]);
+  }, [fetchCollectionsStart]);
 
-  return ( 
+  return (
     <div className="shop-page">
+      <Suspense fallback={<Spinner />}>
       <Route
         exact
         path={`${match.path}`}
@@ -23,6 +27,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
         path={`${match.path}/:collectionId`}
         component={CollectionPageContainer}
       />
+      </Suspense>
     </div>
   );
 };
