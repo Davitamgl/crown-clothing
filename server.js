44bd8218809218ca4c6e374e50c2f312e153e670
Davit Amaghlobeli <amaglobeli53@gmail.com>
2021-08-02 13:38:08 +0400

    added Missing Required Scripts in package.json

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
@@ -14,7 +15,8 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ thrustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
@@ -30,8 +32,8 @@ app.listen(port, (error) => {
   console.log("Server running on port " + port);
 });
 
-app.get("/service-worker.js", (req, req) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post("/payment", (req, res) => {
@@ -42,6 +44,7 @@ app.post("/payment", (req, res) => {
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
+    
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
     } else {
