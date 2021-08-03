14435e2770b61a02d067c446821a0093192c3f1f
Davit Amaghlobeli <amaglobeli53@gmail.com>
2021-08-03 16:12:34 +0400

    fixing service worker bug

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./serviceWorkerRegistration";
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -25,7 +26,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.register();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
